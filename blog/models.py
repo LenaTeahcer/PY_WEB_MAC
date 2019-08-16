2a332012c4a8aa95bd35963df639e4c808024898
LenaTeacher <binjang22@gmail.com>
2019-08-16 16:30:14 +0900

    making error

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
                                    max_length=100, blank=True,
                                    help_text='simple description')
 
-    create_date = models.DateTimeField('Create Date',
+    create_date2 = models.DateTimeField('Create Date',
                                        auto_now_add=True,
                                        blank=True, null=True)
 
